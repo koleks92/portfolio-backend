8942f97dd4b7a1a7fa69f8653c71a16ac7903071
Jan Konieczek <koleks92@gmail.com>
2026-07-16 20:22:41 +0200

    fix: Added app.use(express.json())

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,19 @@ import appsRouter from "./routes/apps.ts";
 import authRouter from "./routes/auth.ts";
 import cors from "cors";
 
-
 const app: Express = express();
 
 app.use(cors());
-
+app.use(express.json());
 
 // Routes
-app.use('/apps', appsRouter);
-app.use('/auth', authRouter)
+app.use("/apps", appsRouter);
+app.use("/auth", authRouter);
 
 app.get("/", async (req: Request, res: Response) => {
-    res.send("Test");
+  res.send("Test");
 });
 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000");
 });
